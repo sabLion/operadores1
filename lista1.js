3687dce45088e39dd568a66d5abdcd17493256b4
sablion <sabrinasuzana07@gmail.com>
2025-01-29 08:20:55 -0300

    Initial commit

---
+++ lista1.js
@@ -0,0 +1,63 @@
+// atv. 1
+let questao = "atv1"
+console.log(questao)
+
+let a = 10
+let b = 5
+
+let soma = b + a
+let sub = a - b 
+let mult = b * a
+let div = a / b
+let resto = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+
+// atv. 2
+let quest = "atv2"
+console.log(quest)
+
+let larg = 20
+let alt = 15
+
+let multi = larg * alt
+
+console.log(multi)
+
+// atv. 3
+let ques = "atv3"
+console.log(ques)
+
+let c = 50
+
+let dobro = c * 2
+let met = c / 2
+
+console.log(dobro)
+console.log(met)
+
+// atv. 4
+let questa= "atv4"
+console.log(questa)
+
+let d = 10
+let e = 15
+let f = 20
+
+let media = (d + e + f) / 3
+
+console.log(media)
+
+// atv. 5
+let q = "atv2"
+console.log(q)
+
+let g = 30
+
+let fahrenheit = (g * 9/5) + 32
+
+console.log(fahrenheit)
